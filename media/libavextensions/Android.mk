005e69a2e3ca54cab0cee52acb457a4a668ad926
Arne Coucheron <arco68@gmail.com>
2017-09-03 06:21:03 +0200

    av: Remove qcom/media/mm-core includes
    This doesn't actually seem to be needed, as code compiles fine
    without it. Remove it so we don't have to wonder about coming
    up with a solution for project-path-for in bp files.
    
    Change-Id: I94e01daec21ea68a0cf9bdcd5d441667cadf28d6

--- media/libavextensions/Android.mk
+++ media/libavextensions/Android.mk
@@ -13,8 +13,7 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/hardware \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/external/flac/include \
-        $(TOP)/system/core/base/include \
-        $(TOP)/hardware/qcom/media/mm-core/inc
+        $(TOP)/system/core/base/include
 
 LOCAL_SHARED_LIBRARIES += \
         libhidlbase \
@@ -54,8 +53,7 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/hardware \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/external/flac/include \
-        $(TOP)/system/core/base/include \
-        $(TOP)/hardware/qcom/media/mm-core/inc
+        $(TOP)/system/core/base/include
 
 LOCAL_SHARED_LIBRARIES += \
         libhidlbase \
